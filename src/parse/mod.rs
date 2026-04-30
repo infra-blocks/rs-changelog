5e55e25dc2dc711b1d67d80ede793e22cfee50de
el-pendeloco <el-pendeloco@users.noreply.github.com>
2026-04-30 11:39:43 -0400

    removed the idea of rules for now
    - It'd be nice to have something configurable, but I think at this time
    it's premature ejaculation. Also, it's totally feasible to add that
    later. For example, "parse" without Rules can return the default
    output of changelog parsing that doesn't list the title as optional. If
    a rule that is added later does make it optional, then the return type
    of the new "parse_with_rules" can reflect that fact mfk.

--- src/parse/mod.rs
+++ src/parse/mod.rs
@@ -1,13 +1,11 @@
 mod changelog;
 mod parser;
-mod rules;
 
 pub use crate::parse::parser::ParseError;
 pub use changelog::Changelog;
 use parser::ChangelogParser;
-pub use rules::Rules;
 
-pub fn parse(source: &str, rules: Rules) -> Result<Changelog<'_>, ParseError> {
-    let parser = ChangelogParser::new(rules);
+pub fn parse(source: &str) -> Result<Changelog<'_>, ParseError> {
+    let parser = ChangelogParser::new();
     parser.parse(source)
 }
